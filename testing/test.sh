3e34976a3b389eae49b0a87ff1797d6354cd4a72
JimmyChin946 <Judes946@gmail.com>
2025-06-24 16:17:06 -0700

    more pokemon test cards

--- testing/test.sh
+++ testing/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -X POST \
-     -F "image=@$HOME/harmony/testing/pokemon.png" \
+     -F "image=@$HOME/harmony/testing/Selection_011.png" \
      -F "product_line_string=pokemon" \
      https://ai.storepass.co/predict
 
