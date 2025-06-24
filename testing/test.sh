3cf1f0128253d315fde6627c9c6db09efc1ca663
JimmyChin946 <judes946@gmail.com>
2025-06-24 20:23:35 +0000

    small logging

--- testing/test.sh
+++ testing/test.sh
@@ -1,4 +1,4 @@
 curl -X POST \
-     -F "image=@$HOME/harmony/testing/pokemon.png" \
+     -F "image=@$HOME/harmony/testing/pokemon3.png" \
      -F "product_line_string=pokemon" \
      https://ai.storepass.co/predict
