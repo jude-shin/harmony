e9ccf24172b236ad39fe5f014bca5ffcbe2f1aa8
JimmyChin946 <Judes946@gmail.com>
2025-06-24 10:32:05 -0700

    skeleton of evaluation

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -1,4 +1,4 @@
-import json, toml, logging, os
+import json, toml, logging, os, typeguard
 
 from pathlib import Path
 from PIL import Image
@@ -11,50 +11,71 @@ from harmony_config.productLines import string_to_productLine
 from utils.data_conversion import label_to_json, format_json
 from helper.image_processing import get_tensor_from_image
 
-from tensorflow.keras import models 
+from tensorflow.keras import models
 
 
 
-# given:
-#   a productline
-# reutrn true if the following are equal (otherwise false):
-#   model outputs layer?
-#   length of the #_labels.toml
-#   config.toml (outputs value)
+    # TODO finish this function
 def validate_outputs(pl: PLS) -> bool:
-    # TODO finish this function 
-    logging.warning('[validate_outputs] not implemented yet. returning true')
-    return True 
+    '''
+    Validates the configuration files in the model directory, making sure the following are correct:
+      model outputs layer?
+      length of the #_labels.toml
+      config.toml (outputs value)
 
-# given:
-#   an image, 
-#   a productline, 
-#   and the submodel type (unique identifier for which (sub)model we are using for evaluation)
-# return the _id of the image 
-def evaluate(image: Image, model_type: int, pl: PLS) -> str:
-    if model_type = 0:
+    Args:
+        pl (PRODUCTLINES): The producteLine we are working with.
+    Returns:
+        bool: Whether all of the checks passed or not
+    '''
+    logging.warning('[validate_outputs] not implemented yet. returning true')
+    return True
+
+def evaluate(image: Image.Image, model_no: int, pl: PLS) -> str:
+    '''
+    Identifies a card.
+
+    Args:
+        image: (Image.Image): The image of the card that is to be identified,
+        pl (PRODUCTLINES): The producteLine we are working with.
+        model_no (int): unique identifier for which (sub)model we are using for evaluation
+    Returns: 
+        str: _id of the image (based on the tcgplayer deckdrafterprod)
+    '''
+    if model_no == 0:
         # validate everything to make sure we aren't going to run into an error
         # could remove for efficiency?
 
+        logging.warning('not implemented')
 
         # the output of this evaluation is going to feed into iteself with a recursive call
     else:
+        logging.warning('not implemented')
         # the output is going to be the real deal (_id)
         # use the master_labels.csv
 
-    # TODO finish this function 
+    # TODO finish this function
     logging.warning('[evaluate] not implemented yet. returning \'0\'')
     return '0'
 
-# given:
-#   a productline
-#   and the submodel type (unique identifier for which (sub)model we are using for evaluation)
-# return the tensorflow keras model
 # TODO: toss this in a utils package if it gets reused later
-def get_model(model_type: int, pl: PLS) -> tf.keras.Model:
-    # TODO: wrap this in a try_catch?
-    model_name = pl.value + '.keras'
-    model_path = os.path.join(os.getenv('MODEL_DIR'), model_name)
-
-    return models.load_model(model_path)
-
+def get_model(model_no: int, pl: PLS) -> models.Model:
+    '''
+    Gets the tensorflow model.
+    Args:
+        pl (PRODUCTLINES): The producteLine we are working with.
+        model_no (int): unique identifier for which (sub)model we are using for evaluation
+    Returns:
+        models.Model: the trained tensorflow model
+    '''
+    try:
+        model_name: str = 'm' + str(model_no) + '.keras'
+        model_dir = os.getenv('MODEL_DIR')
+        if model_dir is None:
+            logging.error('[get_model] MODEL_DIR env var not set')
+            return None
+        model_path = os.path.join(model_dir, pl.value, model_name)
+        return models.load_model(model_path)
+    except Exception as e:
+        logging.error('[get_model] unexpected error %s', e)
+        return None
