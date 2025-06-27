8be5a3a43bfe67ae1c3ec1a783ed1074bf1f38c1
JimmyChin946 <judes946@gmail.com>
2025-06-27 17:02:50 +0000

    no keras files

---
+++ models/pokemon/pop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 0: '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon0Card/POKEMON0_2024.09.16.17.10.35/',
+
+
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon0Card/POKEMON0_2024.09.18.04.20.08/model.keras' '/home/jude/harmony/models/pokemon/m1.keras' 
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon1Card/POKEMON1_2024.09.17.20.46.38/model.keras' '/home/jude/harmony/models/pokemon/m2.keras'  
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon2Card/POKEMON2_2024.09.17.20.59.29/model.keras' '/home/jude/harmony/models/pokemon/m3.keras' 
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon3Card/POKEMON3_2024.09.17.21.26.11/model.keras' '/home/jude/harmony/models/pokemon/m4.keras' 
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon4Card/POKEMON4_2024.09.17.21.30.30/model.keras' '/home/jude/harmony/models/pokemon/m5.keras' 
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon5Card/POKEMON5_2024.09.17.21.35.59/model.keras' '/home/jude/harmony/models/pokemon/m6.keras' 
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon6Card/POKEMON6_2024.09.17.21.58.23/model.keras' '/home/jude/harmony/models/pokemon/m7.keras' 
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon7Card/POKEMON7_2024.09.17.22.12.14/model.keras' '/home/jude/harmony/models/pokemon/m8.keras' 
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon8Card/POKEMON8_2024.09.17.22.14.50/model.keras' '/home/jude/harmony/models/pokemon/m9.keras' 
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon9Card/POKEMON9_2024.09.17.22.18.36/model.keras' '/home/jude/harmony/models/pokemon/m10.keras' 
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon10Card/POKEMON10_2024.09.17.22.29.53/model.keras' '/home/jude/harmony/models/pokemon/m11.keras'
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/Pokemon11Card/POKEMON11_2024.09.18.03.24.08/model.keras' '/home/jude/harmony/models/pokemon/m12.keras'
+
+# overall
+cp '/home/jude/harmony_org/card_recognition_ml/.data/cnn/PokemonCard/POKEMON_2024.09.18.23.43.29/model.keras' '/home/jude/harmony/models/pokemon//m0.keras'
+
+
