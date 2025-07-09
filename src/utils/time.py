ed99a447c125cb18f2519ab4cd2b71c70881f878
JimmyChin946 <judes946@gmail.com>
2025-07-09 16:42:15 +0000

    commenting

--- src/utils/time.py
+++ src/utils/time.py
@@ -3,14 +3,35 @@ from time import localtime, strftime, time
 def get_current_time() -> str:
     '''
     Returns the current time as a nice string.
+
+    Args:
+    Returns:
+        str: the current time as a string 
     '''
+
+
+    '''
+    Convert a given label to the deckdrafterprod _id based on the master_labels.toml
+
+    Args:
+        label (str): what the tensorflow model will spit out
+        pl (PRODUCTLINES): The product_line we are working with.
+    Returns:
+        str: _id that is associated with that label
+    '''
+
     return strftime('%B %d, %I:%M%p', localtime())
 
 
 def get_elapsed_time(start_time: float) -> str:
     '''
-    Using seconds since epoch, determine how much time has passed since the provided float. Returns string
-    with hours:minutes:seconds
+    Using seconds since epoch, determine how much time has passed since the provided float. 
+    At the beginning of the operation you want to time, import time.time(), and that should return a float that you can pass into this function
+    
+    Args:
+        float: the starting time (what time we are referencing)
+    Returns:
+        str: hours:minutes:seconds
     '''
     elapsed_seconds = time() - start_time
     h = int(elapsed_seconds / 3600)
