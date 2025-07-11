52411e51bc2f0b335024354cbb36f50aed6b45f0
JimmyChin946 <judes946@gmail.com>
2025-07-11 18:30:23 +0000

    64 workers is so much faster than 32

--- src/manager.py
+++ src/manager.py
@@ -1,12 +1,18 @@
 import logging
+import time 
 
 from data.collect import download_images_parallel
 from utils.product_lines import PRODUCTLINES as PLS
+from utils.time import get_elapsed_time 
 
 from processing.generate_datasets import generate_datasets
 
 logging.getLogger().setLevel(20)
 
 if __name__ == '__main__':
-    download_images_parallel(pl=PLS.POKEMON, max_workers=32)
+    st = time.time()
+
+    download_images_parallel(pl=PLS.POKEMON, max_workers=64)
+    
+    logging.warning(' ----> ELAPSED TIME: ' + get_elapsed_time(st))
     # generate_datasets(pl=PLS.LORCANA)
