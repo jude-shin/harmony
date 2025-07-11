0a444a79d071f636baea1f0372e13477235c192b
JimmyChin946 <judes946@gmail.com>
2025-07-11 20:44:58 +0000

    removed augmentation for now

--- src/training/train.py
+++ src/training/train.py
@@ -5,6 +5,10 @@
 def train():
     # generate a dataset from the queue
 
+    # NOTE: apply augmentation at runtime for stochastic epochs 
+    # train_dataset = train_dataset.map(apply_augmentation_fn, num_parallel_calls=tf.data.AUTOTUNE)
+
+
 
 
 
