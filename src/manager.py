26400dae125261e52c59f2ee042ed21a99803627
JimmyChin946 <judes946@gmail.com>
2025-07-11 19:10:45 +0000

    generate datasets fully moved

--- src/manager.py
+++ src/manager.py
@@ -1,7 +1,7 @@
 import logging
 import time 
 
-from data.collect import download_images_parallel
+from data.collect import download_images_parallel, collect
 from utils.product_lines import PRODUCTLINES as PLS
 from utils.time import get_elapsed_time 
 
@@ -12,7 +12,9 @@ logging.getLogger().setLevel(20)
 if __name__ == '__main__':
     st = time.time()
 
-    download_images_parallel(pl=PLS.LORCANA, max_workers=64)
-    
+    # download_images_parallel(pl=PLS.LORCANA, max_workers=64)
+    collect(PLS.LORCANA)
+    generate_datasets(pl=PLS.LORCANA)
+
+
     logging.warning(' ----> ELAPSED TIME: ' + get_elapsed_time(st))
-    # generate_datasets(pl=PLS.LORCANA)
