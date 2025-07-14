a4689ca1818b16c92c1099ce97940b7d1b4dc6a7
JimmyChin946 <judes946@gmail.com>
2025-07-14 20:52:17 +0000

    constants updated

--- src/utils/file_handler/dir.py
+++ src/utils/file_handler/dir.py
@@ -1,10 +1,13 @@
 import os
 import logging 
 
-DATA_DIR = 'DATA_DIR'
-SAVED_MODEL_DIR = 'SAVED_MODEL_DIR'
-TRAIN_DATASET_DIR = 'TRAIN_DATASET_DIR'
-VAL_DATASET_DIR = 'VAL_DATASET_DIR'
+from utils.product_lines import PRODUCTLINES as PLS
+
+
+DATA_DIR_ENV= 'DATA_DIR'
+SAVED_MODEL_DIR_ENV = 'SAVED_MODEL_DIR'
+TRAIN_DATASET_PATH_ENV = 'TRAIN_DATASET_PATH_NAME'
+VAL_DATASET_PATH_ENV = 'VAL_DATASET_PATH_NAME'
 
 def get_dir(n: str) -> str:
     d = os.getenv(n)
@@ -16,15 +19,15 @@ def get_dir(n: str) -> str:
     return d
 
 def get_data_dir() -> str:
-    return get_dir(DATA_DIR)
+    return get_dir(DATA_DIR_ENV)
 
 def get_saved_model_dir() -> str:
-    return get_dir(SAVED_MODEL_DIR)
+    return get_dir(SAVED_MODEL_DIR_ENV)
 
-def get_val_dataset_dir() -> str:
-    return get_dir(TRAIN_DATASET_DIR)
+def get_val_dataset_path(pl: PLS) -> str:
+    return os.path.join(get_data_dir(), pl.value, TRAIN_DATASET_PATH_ENV)
 
-def get_train_dataset_dir() -> str:
-    return get_dir(VAL_DATASET_DIR)
+def get_train_dataset_path(pl: PLS) -> str:
+    return os.path.join(get_data_dir(), pl.value, VAL_DATASET_PATH_ENV)
 
 
