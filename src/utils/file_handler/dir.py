c145358dda97e13391cc0d48896cc796d2cd013e
JimmyChin946 <judes946@gmail.com>
2025-07-16 19:45:01 +0000

    env vars

--- src/utils/file_handler/dir.py
+++ src/utils/file_handler/dir.py
@@ -10,7 +10,7 @@ TRAIN_DATASET_PATH_ENV = 'TRAIN_DATASET_PATH_NAME'
 VAL_DATASET_PATH_ENV = 'VAL_DATASET_PATH_NAME'
 IMAGES_PATH_ENV = 'IMAGES_PATH_NAME'
 
-def get_dir(n: str) -> str:
+def get_env(n: str) -> str:
     d = os.getenv(n)
     if d is None:
         msg = n + ' env var is not set. Check the .env file.'
@@ -20,17 +20,16 @@ def get_dir(n: str) -> str:
     return d
 
 def get_data_dir() -> str:
-    return get_dir(DATA_DIR_ENV)
+    return get_env(DATA_DIR_ENV)
 
 def get_saved_model_dir() -> str:
-    return get_dir(SAVED_MODEL_DIR_ENV)
+    return get_env(SAVED_MODEL_DIR_ENV)
 
 def get_val_dataset_path(pl: PLS) -> str:
-    return os.path.join(get_data_dir(), pl.value, VAL_DATASET_PATH_ENV)
+    return os.path.join(get_data_dir(), pl.value, get_env(VAL_DATASET_PATH_ENV))
 
 def get_train_dataset_path(pl: PLS) -> str:
-    return os.path.join(get_data_dir(), pl.value, TRAIN_DATASET_PATH_ENV)
+    return os.path.join(get_data_dir(), pl.value, get_env(TRAIN_DATASET_PATH_ENV))
 
 def get_images_dir(pl: PLS) -> str:
-    return os.path.join(get_data_dir(), pl.value, IMAGES_PATH_ENV)
-
+    return os.path.join(get_data_dir(), pl.value, get_env(IMAGES_PATH_ENV))
