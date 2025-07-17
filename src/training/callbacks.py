04cd184bdd0da0a8a076acdd91dd84fbb46ba0ec
JimmyChin946 <judes946@gmail.com>
2025-07-17 00:40:33 +0000

    callbacks

--- src/training/callbacks.py
+++ src/training/callbacks.py
@@ -1,9 +1,11 @@
 import csv
-import tensorflow as tf
 import gc
+import os
+
+from tensorflow.keras import callbacks, backend
 
 
-class CsvLoggerCallback(tf.keras.callbacks.Callback):
+class CsvLoggerCallback(callbacks.Callback):
     def __init__(self, filename):
         self.filename = filename
 
@@ -16,7 +18,7 @@ class CsvLoggerCallback(tf.keras.callbacks.Callback):
                 writer.writerow(["epoch"] + list(logs.keys()))
             writer.writerow([epoch] + list(logs.values()))
 
-class ValidationAccuracyThresholdCallback(tf.keras.callbacks.Callback):
+class ValidationAccuracyThresholdCallback(callbacks.Callback):
     def __init__(self, threshold):
         super(ValidationAccuracyThresholdCallback, self).__init__()
         self.threshold = threshold
@@ -26,7 +28,39 @@ class ValidationAccuracyThresholdCallback(tf.keras.callbacks.Callback):
             self.model.stop_training = True
 
 
-class ClearMemory(tf.keras.callbacks.Callback):
+class ClearMemory(callbacks.Callback):
     def on_epoch_end(self, batch, logs=None):
-        tf.keras.backend.clear_session()
+        backend.clear_session()
         gc.collect()
+
+
+
+def get_callbacks():
+    # defines when the model will stop training
+    accuracy_threshold_callback = ValidationAccuracyThresholdCallback(
+        threshold=0.98)
+
+    # # saves a snapshot of the model while it is training
+    # checkpoint_filepath = os.path.join(fp["MODEL"], "checkpoint.keras")
+    # checkpoint_callback = callbacks.ModelCheckpoint(
+    #     filepath=checkpoint_filepath, save_weights_only=False, save_best_only=True,
+    #     monitor='val_loss',
+    #     mode='min'
+    # )
+
+    # # logs the epoch, accuracy, and loss for a training session
+    # csv_logger_callback = CsvLoggerCallback(
+    #     os.path.join(fp["MODEL"], "training_logs.csv")
+    # )
+
+    # Define the ReduceLROnPlateau callback
+    reduce_lr_callback = callbacks.ReduceLROnPlateau(
+        monitor="val_loss",  # Metric to monitor
+        factor=0.2,  # Factor by which the learning rate will be reduced
+        patience=5,  # Number of epochs with no improvement after which learning rate will be reduced
+        min_lr=0.00001,  # Lower bound on the learning rate
+    )
+
+    clear_memory_callback = ClearMemory()
+
+    return [accuracy_threshold_callback, reduce_lr_callback]
--- src/training/train.py
+++ src/training/train.py
@@ -2,7 +2,7 @@ import logging
 
 import tensorflow as tf
 
-from tensorflow import keras
+from tensorflow.keras import optimizers, losses, metrics
 
 # from training.callbacks import ValidationAccuracyThresholdCallback
 from data.dataset import load_record
@@ -10,17 +10,19 @@ from cnn.model_structure import CnnModel1
 from utils.file_handler.dir import get_record_path
 from utils.product_lines import PRODUCTLINES as PLS
 
+from training.callbacks import get_callbacks
+
 
 def train(pl: PLS):
     # generate a dataset from the queue
 
     # (1) load the validation and training datasets from the record
     logging.info('Loading Training Dataset from TFRecord...')
-    train_ds = load_record(get_record_path(pl), batch_size=32, shuffle=True, augment=True, multiply=10)
+    train_ds = load_record(get_record_path(pl), batch_size=64, shuffle=True, augment=True, multiply=10)
     logging.info('Finished Loading Training Dataset!')
 
     logging.info('Loading Validation Dataset from TFRecord...')
-    val_ds = load_record(get_record_path(pl), batch_size=32, shuffle=False, augment=False, multiply=1)
+    val_ds = load_record(get_record_path(pl), batch_size=64, shuffle=False, augment=False, multiply=2)
     logging.info('Finished Loading Validation Dataset!')
 
     # (2) load the model
@@ -28,38 +30,24 @@ def train(pl: PLS):
 
     # create object 
     model = CnnModel1([312, 413], 994) # (NOT 993 because one of them were skipped, but we still want that entry...)
-
-    # # normalize layers
-    # model.preprocess.normalize_layer.adapt(train_ds.map(lambda x, y: x))
-
+    
     # build the layers
     model(tf.zeros([1, 312, 413, 3]))
 
-
-    # model.compile(
-    #         optimizer=keras.optimizers.Adam(learning_rate=1e-3),
-    #         loss=keras.losses.BinaryCrossentropy(),
-    #         metrics=[
-    #             keras.metrics.BinaryAccuracy(),
-    #             keras.metrics.FalseNegatives(),
-    #             ],
-    #         )
-
     model.compile(
-        optimizer=keras.optimizers.Adam(learning_rate=1e-3),
-        loss=keras.losses.SparseCategoricalCrossentropy(),
-        metrics=[keras.metrics.SparseCategoricalAccuracy()]
+        optimizer=optimizers.Adam(learning_rate=1e-3),
+        loss=losses.SparseCategoricalCrossentropy(),
+        metrics=[metrics.SparseCategoricalAccuracy()]
     )
-
-
 
     logging.info('Finished Loading Model!')
 
     # (3) model.fit with the custom callbacks
     logging.info('Starting training...')
     model.fit(train_ds,
-              epochs=25,
+              epochs=10000000000000,
               validation_data=val_ds, 
+              callbacks=get_callbacks()
               )
 
 
