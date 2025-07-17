98db335e8fa349983764921cdebb239813eb936f
JimmyChin946 <judes946@gmail.com>
2025-07-17 15:56:36 +0000

    smallg

--- src/manager.py
+++ src/manager.py
@@ -4,7 +4,7 @@ import gc
 
 # import matplotlib
 # matplotlib.use('TkAgg')  
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 import tensorflow as tf 
 
@@ -20,38 +20,38 @@ from data.dataset import augment_blur, augment_saturation, augment_contrast, aug
 
 logging.getLogger().setLevel(20)
 
-def visualize(img_path):
-    label = 0 # dummy label
-    
-    image_raw = tf.io.read_file(img_path)
-    image = tf.image.decode_jpeg(image_raw, channels=3)
-    # image = tf.image.resize(image, [IMG_HEIGHT, IMG_WIDTH])
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    
-    augmentations = [
-        ('Original', lambda img, lbl: (img, lbl)),
-        ('Blur', augment_blur),
-        ('Saturation', augment_saturation),
-        ('Contrast', augment_contrast),
-        ('Hue', augment_hue),
-        ('Brightness', augment_brightness),
-        ('Rotation', augment_rotation),
-        ('Zoom+Rotate', augment_zoom_rotate),
-        ('All Combined', augment_all),
-    ]
-    
-    fig, axs = plt.subplots(2, (len(augmentations) + 1) // 2, figsize=(18, 8))
-    axs = axs.flatten()
-    
-    for idx, (name, fn) in enumerate(augmentations):
-        aug_img, _ = fn(image, label)
-        axs[idx].imshow(tf.clip_by_value(aug_img, 0, 1))
-        axs[idx].set_title(name)
-        axs[idx].axis('off')
-    
-    plt.tight_layout()
-    plt.savefig("augmentation_results.png")
-    # plt.show()
+# def visualize(img_path):
+#     label = 0 # dummy label
+#     
+#     image_raw = tf.io.read_file(img_path)
+#     image = tf.image.decode_jpeg(image_raw, channels=3)
+#     # image = tf.image.resize(image, [IMG_HEIGHT, IMG_WIDTH])
+#     image = tf.image.convert_image_dtype(image, tf.float32)
+#     
+#     augmentations = [
+#         ('Original', lambda img, lbl: (img, lbl)),
+#         ('Blur', augment_blur),
+#         ('Saturation', augment_saturation),
+#         ('Contrast', augment_contrast),
+#         ('Hue', augment_hue),
+#         ('Brightness', augment_brightness),
+#         ('Rotation', augment_rotation),
+#         ('Zoom+Rotate', augment_zoom_rotate),
+#         ('All Combined', augment_all),
+#     ]
+#     
+#     fig, axs = plt.subplots(2, (len(augmentations) + 1) // 2, figsize=(18, 8))
+#     axs = axs.flatten()
+#     
+#     for idx, (name, fn) in enumerate(augmentations):
+#         aug_img, _ = fn(image, label)
+#         axs[idx].imshow(tf.clip_by_value(aug_img, 0, 1))
+#         axs[idx].set_title(name)
+#         axs[idx].axis('off')
+#     
+#     plt.tight_layout()
+#     plt.savefig("augmentation_results.png")
+#     # plt.show()
 
 
 
@@ -65,14 +65,17 @@ if __name__ == '__main__':
     
     # ----------------------------------
     # st = time.time()
-    # collect(PLS.POKEMON)
-    # generate_datasets(PLS.POKEMON)
+    # collect(PLS.LORCANA)
+    # generate_datasets(PLS.LORCANA)
     # logging.warning(' ----> ELAPSED TIME: ' + get_elapsed_time(st))
     # ----------------------------------
     
-    # train(PLS.LORCANA)
+    train(PLS.LORCANA)
 
     # ----------------------------------
-    path = '/home/storepass/harmony/src/Selection_001.png'
-    visualize(path)
+    # path = '/home/storepass/harmony/src/Selection_001.png'
+    # visualize(path)
     # ----------------------------------
+
+
+
