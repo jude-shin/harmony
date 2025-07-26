e73fb5b289362b72312d6547b99b584a0d4ca4df
JimmyChin946 <judes946@gmail.com>
2025-07-26 05:11:26 +0000

    useless

--- src/cnn/model_structure.py
+++ src/cnn/model_structure.py
@@ -55,168 +55,6 @@ class PreprocessingLayer(layers.Layer):
 ##############
 #   BLOCKS   #
 ##############
-@saving.register_keras_serializable(package='cnn')
-class ConvBlock(layers.Layer):
-    '''
-    A modular convolutional block: Conv2D -> BatchNorm -> ReLU -> MaxPool
-    '''
-    def __init__(self, filters, kernel_size, pool_size, **kwargs):
-        super().__init__(**kwargs)
-        self.filters = filters
-        self.kernel_size = kernel_size
-        self.pool_size = pool_size
-
-        self.conv = layers.Conv2D(filters, kernel_size, padding='same')
-        self.bn = layers.BatchNormalization()
-        self.act = layers.ReLU() # could be LeakyReLu
-        self.pool = layers.MaxPooling2D(pool_size)
-
-    def call(self, inputs, training=False):
-        x = self.conv(inputs)
-        x = self.bn(x, training=training)
-        x = self.act(x)
-        return self.pool(x)
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            'filters': self.filters,
-            'kernel_size': self.kernel_size,
-            'pool_size': self.pool_size,
-            'conv': self.conv,
-            'bn': self.bn,
-            'act': self.act,
-            'pool': self.pool,
-            })
-        return config
-
-    @classmethod
-    def from_config(cls, config):
-        # conv = layers.deserialize(config.pop('conv'))
-        # bn = layers.deserialize(config.pop('bn'))
-        # act = layers.deserialize(config.pop('act'))
-        # pool = layers.deserialize(config.pop('pool'))
-    
-        instance = cls(**config)
-        # instance.conv = conv
-        # instance.bn = bn
-        # instance.act = act
-        # instance.pool = pool
-        return instance
-
-
-@saving.register_keras_serializable(package='cnn')
-class SEBlock(layers.Layer):
-    def __init__(self, ratio, **kw):
-        super().__init__(**kw)
-        self.ratio = ratio
-        self.global_avg_pool = layers.GlobalAveragePooling2D()
-        self.dense1 = None
-        self.dense2 = None
-        self.reshape = None
-
-    def call(self, inputs):
-        x = self.global_avg(inputs)
-        x = self.dense1(x)
-        x = self.dense2(x)
-        x = self.reshape(x)
-        return inputs * x
-
-    def build(self, input_shape):
-        ch = input_shape[-1]
-        self.dense1 = layers.Dense(ch // self.ratio, activation="relu")
-        self.dense2 = layers.Dense(ch, activation="sigmoid")
-        self.reshape = layers.Reshape((1, 1, ch))
-        super().build(input_shape)
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            'ratio': self.ratio,
-            })
-        return config
-
-    @classmethod
-    def from_config(cls, config):
-        return cls(**config)
-
-@saving.register_keras_serializable(package='cnn')
-class ResidualBlock(layers.Layer):
-    def __init__(self, filters, kernel_size, **kwargs):
-        super().__init__(**kwargs)
-        self.filters = filters
-        self.kernel_size = kernel_size
-
-        self.conv1 = layers.Conv2D(filters, kernel_size, padding='same')
-        self.bn1 = layers.BatchNormalization()
-        self.relu = layers.ReLU()
-        self.conv2 = layers.Conv2D(filters, kernel_size, padding='same')
-        self.bn2 = layers.BatchNormalization()
-
-    def call(self, inputs, training=False):
-        x = self.conv1(inputs)
-        x = self.bn1(x, training=training)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x, training=training)
-        return self.relu(x + inputs)
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            'filters': self.filters,
-            'kernel_size': self.kernel_size,
-            'conv1': self.conv1,
-            'bn1': self.bn1,
-            'relu': self.relu,
-            'conv2': self.conv2,
-            'bn2': self.bn2,
-            })
-        return config
-
-    @classmethod
-    def from_config(cls, config):
-        # conv1 = layers.deserialize(config.pop('conv1'))
-        # bn1 = layers.deserialize(config.pop('bn1'))
-        # relu = layers.deserialize(config.pop('relu'))
-        # conv2 = layers.deserialize(config.pop('conv2'))
-        # bn2 = layers.deserialize(config.pop('bn2'))
-    
-        instance = cls(**config)
-        # instance.conv1 = conv1
-        # instance.bn1 = bn1
-        # instance.relu = relu
-        # instance.conv2 = conv2
-        # instance.bn2 = bn2
-        return instance
-
-
-@saving.register_keras_serializable(package='cnn')
-class DropBlock(layers.Layer):
-    def __init__(self, rate, **kwargs):
-        super().__init__(**kwargs)
-        self.rate = rate
-
-        self.drop = layers.SpatialDropout2D(rate)
-
-    def call(self, inputs, training=False):
-        return self.drop(inputs, training=training)
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            'rate': self.rate,
-            'drop': self.drop,
-            })
-        return config
-
-    @classmethod
-    def from_config(cls, config):
-        # drop = layers.deserialize(config.pop('drop'))
-        instance = cls(**config)
-        # instance.drop = drop
-        return instance
-
 
 @saving.register_keras_serializable(package='cnn')
 class ConvBnLeakyBlock(layers.Layer):
@@ -248,137 +86,21 @@ class ConvBnLeakyBlock(layers.Layer):
             'kernel_size': self.kernel_size,
             'pool_size': self.pool_size,
             'l2': self.l2,
-            'conv': self.conv,
-            'bn': self.bn,
-            'act': self.act,
-            'pool': self.pool,
+            # 'conv': self.conv,
+            # 'bn': self.bn,
+            # 'act': self.act,
+            # 'pool': self.pool,
             })
         return config
 
     @classmethod
     def from_config(cls, config):
-        # conv = layers.deserialize(config.pop('conv'))
-        # bn = layers.deserialize(config.pop('bn'))
-        # act = layers.deserialize(config.pop('act'))
-        # pool = layers.deserialize(config.pop('pool'))
-    
-        instance = cls(**config)
-        # instance.conv = conv
-        # instance.bn = bn
-        # instance.act = act
-        # instance.pool = pool
-        return instance
+        return cls(**config)
 
-# @saving.register_keras_serializable(package='cnn')
-# class DenseDropoutBlock(layers.Layer):
-#     '''
-#     Dense + Dropout with L2 regularization, for classifier head.
-#     '''
-#     def __init__(self, units, dropout_rate=0.5, l2=0.01, **kwargs):
-#         super().__init__(**kwargs)
-#         self.dense = layers.Dense(units, activation='relu', kernel_regularizer=regularizers.l2(l2))
-#         self.dropout = layers.Dropout(dropout_rate)
-# 
-#     def call(self, inputs, training=False):
-#         x = self.dense(inputs)
-#         return self.dropout(x, training=training)
-# 
-#     def get_config(self):
-#         config = super().get_config()
-#         config.update({
-#             'dense': self.dense,
-#             'dropout': self.dropout,
-#             })
-#         return config
-# 
-#     @classmethod
-#     def from_config(cls, config):
-#         dense = layers.deserialize(config.pop('dense'))
-#         dropout = layers.deserialize(config.pop('dropout'))
-#     
-#         instance = cls(**config)
-#         instance.dense = dense
-#         instance.dropout = dropout
-#         return instance
-
-####################
-#   BLOCK MACROS   #
-####################
-
-def makeCnnSeq(input_shape, filters, dropout_rate):
-    return Sequential([
-        ConvBlock(filters=filters, kernel_size=3, pool_size=2),
-        ResidualBlock(filters, kernel_size=3),
-        SEBlock(8),
-        DropBlock(rate=dropout_rate)
-        ])
 
 #######################
 #   MODEL TEMPLATES   #
 #######################
-# @saving.register_keras_serializable()
-# class CnnModel1(Model):
-#     def __init__(self, input_shape, num_classes, **kwargs):
-#         super().__init__(**kwargs)
-#         self.input_shape = input_shape
-#         self.num_classes = num_classes 
-# 
-#         self.preprocess = PreprocessingLayer(target_size=input_shape[1:3])
-# 
-#         self.blocks = [
-#                 makeCnnSeq(input_shape, 32, 0.2),
-#                 makeCnnSeq(input_shape, 64, 0.3),
-#                 makeCnnSeq(input_shape, 128, 0.4),
-#                 ]
-# 
-#         self.pool = layers.GlobalAveragePooling2D()
-#         self.output_layer = layers.Dense(num_classes, activation='softmax')
-# 
-#     def call(self, inputs, training=False):
-#         x = self.preprocess(inputs)
-# 
-#         for block in self.blocks: 
-#             x = block(x, training=training)
-# 
-#         x = self.pool(x)
-#         return self.output_layer(x)
-# 
-#     def build(self, input_shape):
-#         logging.info('INPUT SHAPE')
-#         logging.info(input_shape)
-#         dummy_input = tf.zeros(input_shape)
-#         self.call(dummy_input, training=False)
-#         super().build(input_shape)
-# 
-#     def get_config(self):
-#         config = super().get_config()
-#         config.update({
-#             'input_shape': self.input_shape,
-#             'preprocess': self.preprocess,
-#             'blocks': self.blocks,
-#             'pool': self.pool,
-#             'output_layer': self.output_layer,
-#             })
-#         return config
-# 
-#     @classmethod
-#     def from_config(cls, config):
-#         preprocess = layers.deserialize(config.pop('preprocess'))
-#         blocks = [layers.deserialize(block_cfg) for block_cfg in config.pop('blocks')]
-#         pool = layers.deserialize(config.pop('pool'))
-#         output_layer = layers.deserialize(config.pop('output_layer'))
-#         input_shape = [1, *config.pop('input_shape')]
-#     
-#         instance = cls(input_shape=input_shape, num_classes=output_layer.units, **config)
-#         instance.preprocess = preprocess
-#         instance.blocks = blocks
-#         instance.pool = pool
-#         instance.output_layer = output_layer
-# 
-#         instance.build(input_shape)
-#         return instance
-
-
 @saving.register_keras_serializable()
 class CnnModelClassic15Mini(Model):
     '''
@@ -430,43 +152,12 @@ class CnnModelClassic15Mini(Model):
         config.update({
             'input_shape': self.input_shape,
             'num_classes': self.num_classes,
-            # 'preprocess': self.preprocess,
-            # 'blocks': self.blocks,
-            # 'global_pool': self.global_pool,
-            # 'hidden': self.hidden,
-            # 'dropout': self.dropout,
-            # 'output_layer': self.output_layer,
             })
         return config
 
     @classmethod
     def from_config(cls, config):
-        # preprocess = layers.deserialize(config.pop('preprocess'))
-        # blocks = [layers.deserialize(cfg) for cfg in config.pop('blocks')]
-        # global_pool = layers.deserialize(config.pop('global_pool'))
-        # hidden = layers.deserialize(config.pop('hidden'))
-        # dropout = layers.deserialize(config.pop('dropout'))
-        # output_layer = layers.deserialize(config.pop('output_layer'))
-
-        # input_shape = config.pop('input_shape')
-        # input_shape = [1, *input_shape[-3:]]
-    
-        # instance = cls(input_shape=input_shape, num_classes=output_layer.units, **config)
-        # instance.preprocess = preprocess
-        # instance.blocks = blocks
-        # instance.global_pool = global_pool
-        # instance.hidden = hidden
-        # instance.dropout = dropout
-        # instance.output_layer = output_layer
-
-        # instance.build(input_shape)
-
-
-
-        instance = cls(**config)
-
-        return instance
-
+        return cls(**config)
 
 
 # @saving.register_keras_serializable()
