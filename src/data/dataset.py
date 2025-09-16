d58c7c2652c328b6462f45cf7bb55bccde65d476
JimmyChin946 <judes946@gmail.com>
2025-09-16 16:56:28 +0000

    I am a donut

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -163,7 +163,7 @@ def load_record(pl: PLS, batch_size, shuffle, multiply, num_classes, img_height,
     if multiply > 1:
         ds = ds.flat_map(lambda x, y: tf.data.dataset.Dataset.from_tensors((x, y)).repeat(multiply))
 
-    if shuffle: ds = ds.shuffle(buffer_size=(256, batch_size * multiply * 4)) # try and get in multiple batches
+    if shuffle: ds = ds.shuffle(buffer_size=max(256, batch_size * multiply * 4)) # try and get in a larger batch (default 256)
 
     ds = ds.batch(batch_size)
     ds = ds.map(lambda x, y: (x, tf.one_hot(y, depth=num_classes)), num_parallel_calls=tf.data.AUTOTUNE)
