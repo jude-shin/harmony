f248d08e416d0327512ac2b3fe40918650f62372
JimmyChin946 <Judes946@gmail.com>
2025-10-01 11:25:20 -0700

    starting to remove storepass_api

--- src/serving_api/main.py
+++ src/serving_api/main.py
@@ -91,20 +91,3 @@ async def predict(
             "confidences": confidences,
             # threshold is currently unused in this handler; left as part of API for downstream logic
             }
-
-
-from typing import Union
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
