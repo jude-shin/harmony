096d31e2bbb2c8caa6e5f490469a2db5f96d9ecd
JimmyChin946 <Judes946@gmail.com>
2025-10-01 15:27:49 -0700

    typehints becuase we want to write good code

--- src/utils/file_handler/toml.py
+++ src/utils/file_handler/toml.py
@@ -1,21 +1,21 @@
+import os
 import toml
-import tomllib 
+import tomllib
 
 from utils.product_lines import PRODUCTLINES as PLS
-from utils.file_handler.dir import * 
+from utils.file_handler.dir import get_config_path
 
 # TODO: rename this package to "config" or something
-def load_model_config(pl: PLS) -> dict:
-    path = get_config_path(pl)
+def load_model_config(pl: PLS) -> dict[str, str | int | bool | list[str]]:
+    # string, int, float, dict of strings, boolean
+    path: str = get_config_path(pl)
 
     with open(path, 'rb') as f:
         config = tomllib.load(f)
 
-    return config 
+    return config
 
-def save_model_config(new_config_path: str, model: str, config: dict) -> dict:
+def save_model_config(new_config_path: str, model: str, config: dict[str, str | int | bool | list[str]]) -> None:
     new_config_path = os.path.join(new_config_path, model+'.toml')
     with open(new_config_path, 'w+') as f:
-        toml.dump(config, f) 
-
-    return config 
+        _ = toml.dump(config, f)
